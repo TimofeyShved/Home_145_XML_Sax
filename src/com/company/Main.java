964ac502a72d20969f6e31be24fa0329192193db
TimofeyShved <timofeya123@inbox.ru>
2023-12-10 15:38:06 +0600

    Stream

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,14 +7,23 @@ import org.xml.sax.helpers.DefaultHandler;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Main {
 
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException, XMLStreamException {
 
-        //Настройки для парсинга
+        // Что бы прочитать XML файл, нужно создать сборщик ( ◡‿◡ *)
+        SAXParserFactory factory1 = SAXParserFactory.newInstance();
+        SAXParser parser1 = factory1.newSAXParser();
+
+        //Настройки для парсинга ╰(*´︶`*)╯
         DefaultHandler handler = new DefaultHandler(){
             @Override
             public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
@@ -37,13 +46,25 @@ public class Main {
             }
         };
 
-        // Что бы прочитать XML файл, нужно создать сборщик
-        SAXParserFactory factory = SAXParserFactory.newInstance();
-        SAXParser parser = factory.newSAXParser();
-        parser.parse(new File("text.xml"), handler);
+        // --------------- 1 вариант
+        parser1.parse(new File("text.xml"), handler);
 
+        System.out.println("--------------- --------------- --------------- --------------- ");
 
+        // --------------- либо 2 вариант (ﾉ´ヮ`)ﾉ*: ･ﾟ
+        XMLInputFactory factory2 = XMLInputFactory.newFactory();
+        XMLStreamReader parser2 =  factory2.createXMLStreamReader(new FileInputStream("text.xml"));
 
+        while (parser2.hasNext()){
+            int event = parser2.next();
+            if (event == XMLStreamConstants.START_ELEMENT){
+                if (parser2.getAttributeValue(0) != null)
+                    System.out.print(parser2.getAttributeValue(0));
+            }
+            if (event == XMLStreamConstants.CHARACTERS){
+                System.out.print(parser2.getText());
+            }
+        }
 
     }
 }
